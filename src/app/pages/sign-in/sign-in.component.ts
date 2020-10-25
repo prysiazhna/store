bdc5dc0639214d3048818bf461e7f874e2ad6c2e
prysiazhna <olha.prysiazhna@gmail.com>
2020-10-26 00:22:11 +0200

    more

--- src/app/pages/sign-in/sign-in.component.ts
+++ src/app/pages/sign-in/sign-in.component.ts
@@ -4,18 +4,14 @@ import { FormBuilder, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { IStore } from 'src/app/store/reducers';
-import { AngularFireAuth } from '@angular/fire/auth';
 import { AbstractForm } from 'src/app/shared/form-helper';
 @Component({
-  // tslint:disable-next-line:component-selector
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.scss'],
 })
 export class SignInComponent extends AbstractForm implements OnInit {
-  public constructor(private fb: FormBuilder,
-     private store: Store<IStore>
-     ) {
+  public constructor(private fb: FormBuilder, private store: Store<IStore>) {
     super();
   }
 
